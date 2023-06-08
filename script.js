dedbcf6164ac487b44dfb1b1c0c17fd1f75a1563
Flavio Silva <flavio.silvapinkman@gmail.com>
2023-06-08 20:21:11 -0300

    bug solved

---
+++ script.js
@@ -0,0 +1,42 @@
+//var
+const musica = document.querySelector('audio')
+//events
+document.querySelector('.botao-play').addEventListener('click', tocarMusica)
+
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica)
+
+musica.addEventListener('timeupdate', atualizarBarra)
+
+//functions
+
+function tocarMusica(){
+    musica.play()
+    document.querySelector('.botao-pause').style.display = 'block'
+    document.querySelector('.botao-play').style.display = 'none'
+}
+
+function pausarMusica(){
+    musica.pause()
+    document.querySelector('.botao-pause').style.display = 'none'
+    document.querySelector('.botao-play').style.display = 'block'
+}
+
+function atualizarBarra(){
+    const barra = document.querySelector('progress')
+
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%'
+
+    const tempoDecorrido = document.querySelector('.inicio')
+    
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime))
+}
+
+function segundosParaMinutos(segundos){
+    let campoMinutos = Math.floor(segundos / 60);
+    let campoSegundos = segundos % 60;
+    if (campoSegundos < 10){
+        campoSegundos = '0' + campoSegundos;
+    }
+
+    return campoMinutos+':'+campoSegundos;
+}
